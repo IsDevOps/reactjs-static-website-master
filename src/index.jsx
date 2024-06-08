4f9465ffe754698570a8ff05a9b86674ac6bdbd6
IsDevOps <issyoseghale@gmail.com>
2024-06-08 21:19:09 +0000

    commited

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+ReactDOM.render(<App />, document.getElementById('root'),
+<iframe src="https://www.example.com/map" title="Interactive Map"></iframe>
+);
